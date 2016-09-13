11419a6be9cbac80af072844e7cc84d2e6240d59
musyoku <musyoku@users.noreply.github.com>
2016-09-13 22:34:43 +0900

    j

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-This code is under development.
-We are currently preparing an experiment.
+## Auxiliary Deep Generative Models
+
+This is the Chainer implementation of [Auxiliary Deep Generative Models [arXiv:1602.05473]](http://arxiv.org/abs/1602.05473)
+
+[この記事](http://musyoku.github.io/2016/09/10/Auxiliary-Deep-Generative-Models/)で実装したコードです。
+
+See also:
+- [VAE](https://github.com/musyoku/variational-autoencoder)
+- [AAE](https://github.com/musyoku/adversarial-autoencoder)
+
+### Requirements
+
+- Chainer 1.12+
+- Pillow
+- Pylab
+- matplotlib.patches
+- pandas
+
+
+## Runnning
+
+### Download MNIST
+
+run `mnist-tools.py` to download and extract MNIST.
+
+### ADGM
+
+run `train_adgm/train.py`
+
+### SDGM
+
+run `train_sdgm/train.py`
+
+## Validation Accuracy
+
+![acuracy](http://musyoku.github.io/images/post/2016-09-10/adgm_graph.png)
+
+## Analogies
+
+### ADGM
+
+![analogy](http://musyoku.github.io/images/post/2016-09-10/analogy_adgm.png)
+
+### SDGM
+
+![analogy](http://musyoku.github.io/images/post/2016-09-10/analogy_sdgm.png)
--- train_sdgm/train.py
+++ train_sdgm/train.py
@@ -14,10 +14,6 @@ def sample_labeled_data():
 	# binalize
 	x_labeled = util.binarize_data(x_labeled)
 
-	x_labeled = Variable(x_labeled)
-	y_labeled = Variable(y_labeled)
-	label_ids = Variable(label_ids)
-
 	return x_labeled, y_labeled, label_ids
 
 def sample_validation_data():
@@ -26,9 +22,6 @@ def sample_validation_data():
 	# binalize
 	x_labeled = util.binarize_data(x_labeled)
 
-	x_labeled = Variable(x_labeled)
-	label_ids = Variable(label_ids)
-
 	return x_labeled, label_ids
 
 def sample_unlabeled_data():
@@ -36,8 +29,6 @@ def sample_unlabeled_data():
 
 	# binalize
 	x_unlabeled = util.binarize_data(x_unlabeled)
-
-	x_unlabeled = Variable(x_unlabeled)
 
 	return x_unlabeled
 
@@ -90,7 +81,7 @@ for epoch in xrange(max_epoch):
 		x_unlabeled = sample_unlabeled_data()
 
 		# train
-		loss_labeled, loss_unlabeled = sdgm.train(x_labeled, y_labeled, label_ids, x_unlabeled)
+		loss_labeled, loss_unlabeled = sdgm.train(x_labeled, y_labeled, x_unlabeled)
 		loss_classifier = sdgm.train_classification(x_labeled, label_ids, alpha=alpha)
 
 		sum_loss_labeled += loss_labeled
@@ -109,10 +100,13 @@ for epoch in xrange(max_epoch):
 
 	# validation
 	x_labeled, label_ids = sample_validation_data()
+	x_labeled = Variable(x_labeled)
+	if conf.gpu_enabled:
+		x_labeled.to_gpu()
 	predicted_ids = sdgm.sample_x_label(x_labeled, test=True, argmax=True)
 	correct = 0
 	for i in xrange(n_validation_data):
-		if predicted_ids[i] == label_ids.data[i]:
+		if predicted_ids[i] == label_ids[i]:
 			correct += 1
 	print "classification accuracy (validation): {}".format(correct / float(n_validation_data))
 
