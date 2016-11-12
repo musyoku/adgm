385e50debb2b8f3db34826c8a1c3608cd8f2ebcf
musyoku <musyoku@users.noreply.github.com>
2016-11-13 01:59:29 +0900

    j

--- mnist_tools.py
+++ mnist_tools.py
@@ -24,7 +24,7 @@ def load_mnist(data_filename, label_filename, num):
 			for j in six.moves.range(dim):
 				images[i, j] = ord(f_images.read(1))
 
-			if i % 99 == 0 or i == num - 1:
+			if i % 100 == 99 or i == num - 1:
 				sys.stdout.write("\rloading images ... ({} / {})".format(i + 1, num))
 				sys.stdout.flush()
 	sys.stdout.write("\n")
